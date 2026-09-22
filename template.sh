831a0ca2c82f436c98d3519297a5393b0b16c962
“Harshit <harshitsinghunnao@gmail.com>
2026-09-22 22:59:27 +0530

    folder added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+#crating files
+touch src/_init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully."
